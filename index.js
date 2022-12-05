00e91d47d73216f68ba98663b63ecc15a77778f1
Your Name <you@example.com>
2022-12-05 21:13:02 +0530

    complited

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let music = new Audio("music.mp3");
 let ting = new Audio("ting.mp3");
 let gameOver = new Audio("gameover.mp3");
 let turn = "X";
+let isgameOver = false;
 
 //function to change the turn 
 const changeTurn = () =>{
@@ -11,22 +12,53 @@ const changeTurn = () =>{
 
 //function to check for a win 
 const checkWin = () =>{
-
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    wins.forEach(e => {
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText === boxtext[e[2]].innerText) && boxtext[e[0]].innerText !== ""){
+            console.log(boxtext[e[0]].innerText);
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + "   Won";
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            isgameOver = true;
+        }
+    })
 }
 
 // game logic 
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
     let boxtext = element.querySelector('.boxtext');
-    console.log(boxtext);
+   // console.log(boxtext);
     element.addEventListener('click', (e)=>{
         if(boxtext.innerText === ''){
-            console.log("x");
+           // console.log("x");
             boxtext.innerText = turn;
-            changeTurn();
+            turn = changeTurn();
             ting.play();
             checkWin();
-            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn; 
+            if(!isgameOver){                
+            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;} 
         }
     })
 })
+
+reset.addEventListener('click', ()=>{
+    let boxtext = document.querySelectorAll('.boxtext');
+    Array.from(boxtext).forEach(element =>{
+        element.innerText = "";
+    });
+    turn = "X";
+    isgameOver = false;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = " 0px";
+})
+ 
